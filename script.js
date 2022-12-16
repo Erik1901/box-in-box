c152a8fc9daeee313e6e213ffcf3cc0c1e6c6957
Erik1901 <coolaerik700@gmail.com>
2022-12-16 16:33:24 +0100

    Finare kod

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-const smallbox = document.getElementById('smallbox');
-var bordercollision = 450;
+const SMALLBOX = document.getElementById("smallbox");
+var borderCollision = 450;
 
-smallbox.style.top = '0px';
-smallbox.style.left = '0px';
-smallbox.style.height = '50px';
-smallbox.style.width = '50px';
+SMALLBOX.style.top = "0px";
+SMALLBOX.style.left = "0px";
+SMALLBOX.style.height = "50px";
+SMALLBOX.style.width = "50px";
 
 function up() {
-    var top = parseInt(smallbox.style.top);
-    if (top > 0) {
-        smallbox.style.top = (top - 10) + 'px';
-    }
+  var top = parseInt(SMALLBOX.style.top);
+  if (top > 0) {
+    SMALLBOX.style.top = top - 10 + "px";
+  }
 }
 
 function down() {
-    var top = parseInt(smallbox.style.top);
-    if (top < bordercollision) {
-        smallbox.style.top = (top + 10) + 'px';
-    }
+  var top = parseInt(SMALLBOX.style.top);
+  if (top < borderCollision) {
+    SMALLBOX.style.top = top + 10 + "px";
+  }
 }
 
 function left() {
-    var left = parseInt(smallbox.style.left);
-    if (left > 0) {
-        smallbox.style.left = (left - 10) + 'px';
-    }
+  var left = parseInt(SMALLBOX.style.left);
+  if (left > 0) {
+    SMALLBOX.style.left = left - 10 + "px";
+  }
 }
 
 function right() {
-    var left = parseInt(smallbox.style.left);
-    if (left < bordercollision) {
-        smallbox.style.left = (left + 10) + 'px';
-    }
+  var left = parseInt(SMALLBOX.style.left);
+  if (left < borderCollision) {
+    SMALLBOX.style.left = left + 10 + "px";
+  }
 }
